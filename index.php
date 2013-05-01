003439685996df0a329c3c00f732dc828238690d
Tamerlan <tamerjlah@gmail.com>
2013-05-01 06:38:01 -0400

    bg canvas example

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php //CONSTANTS
-	// "Server details"
+    // "Server details"
 	define ('NAME',$_SERVER['SERVER_NAME']  );
 	define ('ADDR',$_SERVER['SERVER_ADDR']  );
 	define ('PORT',$_SERVER['SERVER_PORT']  );
@@ -37,30 +37,60 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <title>Canvas banner</title>
   <meta name="description" content="My testing page">
-  <?/*
-    <!-- Mobile viewport optimized: h5bp.com/viewport -->
-    <!-- DEVICE CONFIG -->
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" /><!-- define width & scale attr-->
-		<meta name="apple-mobile-web-app-capable" 			content="yes" /> <!-- full screen -->
-		<meta name="apple-mobile-web-app-status-bar-style" 	content="black-translucent"> <!-- clock bar color -->
-		<meta name="format-detection" content="telephone=no">   
-	
-	<!-- ICONS -->
-	 <!-- Place favicon.ico and apple-touch-icon.png in the root directory: mathiasbynens.be/notes/touch-icons -->
-		<link rel="apple-touch-icon" href="<?=ebIMG?>dt_icon.png" />
-		<link rel="apple-touch-startup-image" href="<?=ebIMG?>load_screen.png" /> 
-		 
-		<link rel="apple-touch-icon" sizes="72x72" href="touch-icon-ipad.png" />
-		<link rel="apple-touch-icon" sizes="114x114" href="touch-icon-iphone4.png" />
-   */?><meta name="viewport" content="width=device-width">
+  <meta name="viewport" content="width=device-width">
 	  
 	  <!-- CSS -->
-	  <link href="<?=CSS?>base_style.css" 	rel="stylesheet" type="text/css">
-	  <link href="<?=CSS?>fields.css" 		rel="stylesheet" type="text/css">
+	  <link href="<?php echo CSS?>base_style.css" 	rel="stylesheet" type="text/css">
+	  <link href="<?php echo CSS?>fields.css" 		rel="stylesheet" type="text/css">
 	  <link rel="stylesheet" href="css/style.css">
 		
 	  <!-- SCRIPT CONSTANTS (JS,JQ,JQM) --> 
 	  <script src="js/libs/modernizr-2.5.3.min.js"></script>
+      
+      
+      <link href='http://fonts.googleapis.com/css?family=Open+Sans|Source+Code+Pro' rel='stylesheet' type='text/css'>
+      <style>
+      #wrapper {
+            text-align: center;
+        }
+        #wrapper > div {
+        	display: inline-block;
+        	margin: 0 20px;
+        	padding: 10px;
+        	background-color: #eee;
+        	border-radius: 10px;
+        	font-weight: 600;
+        }
+        #container {
+          width: 300px;
+          height: 300px;
+          /*https://www.webkit.org/blog/176/css-canvas-drawing/*/
+          background: -webkit-canvas(animation) no-repeat 50% 50%;
+          background-size: 100%;
+          display: inline-block;
+          -webkit-transition: background 0.4s;
+        }
+        pre.prettyprint {
+        	font-family: 'Source Code Pro', sans-serif;
+        	width: auto;
+        	display: inline-block;
+        	text-align: left;
+        	margin-top: 30px;
+        }
+        #notsupport {
+          text-align: center;
+          font-weight: bold;
+          color: red;
+          padding: 10px;
+          border: 1px solid red;
+          background-color: rgba(255,0,0,0.1);
+          border-radius: 5px;
+          display: none;
+          position: relative;
+          z-index: 1000;
+        }
+      
+      </style>
 </head>
 
 <body onload="init()">
@@ -70,11 +100,49 @@
   <header>
 
   </header>
-  <div role="main">
-	hello world!
-	</br>
+  <div style="float:left;width:100%" role="main">
+    <div style="float left">
+      
+        <details>
+          <summary>What's this?</summary>
+          <div>
+            <p>Drawing to <code>&lt;canvas></code> and using it as the CSS background of a DOM node. This allows programmatic animations for background images. Most people use hacks using <code>.toDataURL()</code> to do the same, but <code>.toDataURL()</code> adds a ~33% overhead to the resulting image and involves touching the DOM in JS (<code>el.style.background = 'url("data:...")';</code>). Both are inefficient.</p>
+        
+            	<p>Instead of specifying a URL for the <code>background-image</code>, reference the same 
+        		identifier/name in <code>-webkit-canvas()</code> as the one used in the call to <code>document.getCSSCanvasContext()</code>. Note: this is a special method that you need to create
+        		the 2d context with.</p>
+        		
+        		<p>Once things are hooked up, <code>requestAniamtionFrame()</code> is used to drive the canvas animation.
+        		The rest is taken care of by the browser when the association is made.</p>
+          </div>
+        </details>
+
+        <div id="notsupport">Your browser doesn't support document.getCSSCanvasContext</div>
+
+        <div id="wrapper">
+        	<p>Drawing an animation to <code>&lt;canvas></code> and using it as the CSS <code>background-image</code> of a DOM node.<br>This allows backgrounds to be "live" and powered programmatically via canvas.</p> 
+        <div>
+    	    <div id="container"></div>
+    	    <div>&lt;div class="canvas-bg"&gt;</div>
+        </div>
+        <div id="canvas-container">
+        	<div>&lt;canvas&gt;</div>
+        </div>
+        
+        <pre class="prettyprint">
+        &lt;style&gt;
+          .canvas-bg {
+            background: -webkit-canvas(animation) no-repeat 50% 50%;
+          }
+        &lt;/style&gt;
+        &lt;script&gt;
+          var ctx = document.getCSSCanvasContext('2d', 'animation', 300, 300);
+        &lt;/script&gt;
+        </pre>
+        </div>
+    </div>
 	
-	<canvas id="testCanvas" width="800" height="800">
+	<canvas style="float:left;clear:both;margin-left:33%;margin-top:40px" id="testCanvas" width="500" height="500">
 		Canvas not supported
 	</canvas>
 	
@@ -85,15 +153,15 @@
 
   <!-- JavaScript at the bottom for fast page loading -->
 	<?php /*   	<script src="<=JQM?>jquery.mobile-1.1.0.min.js" type="text/javascript"></script>
-      <script src="<?=JQ?>jquery.iosslider.min.js"    type="text/javascript"></script>  */?>
+      <script src="<?php echo JQ?>jquery.iosslider.min.js"    type="text/javascript"></script>  */?>
 	
   <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
-	  <script src="<?=JQext?>"></script> 
-	  <script>window.jQuery || document.write('<script src="<?=JQ?>jquery.min.js"><\/script>')</script>
+	  <script src="<?php echo JQext?>"></script> 
+	  <script>window.jQuery || document.write('<script src="<?php echo JQ?>jquery.min.js"><\/script>')</script>
 	  
-	  <script src="<?=JS?>underscore.min.js"          type="text/javascript"></script>
-	  <script src="<?=BB?>backbone.min.js"            type="text/javascript"></script>
-	  <script src="<?=BB?>localStorage.js"            type="text/javascript"></script>
+	  <script src="<?php echo JS?>underscore.min.js"          type="text/javascript"></script>
+	  <script src="<?php echo BB?>backbone.min.js"            type="text/javascript"></script>
+	  <script src="<?php echo BB?>localStorage.js"            type="text/javascript"></script>
 	  
 		<!-- scripts concatenated and minified via build script -->
 	  <script src="js/plugins.js"></script>
--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ var bufferCanvasCtx = null;
 var flakeArray = [];
 var flakeTimer = null;
 var maxFlakes = 200;
+var ctx = null;;
 
 function Flake() {
 	this.x = Math.round(Math.random() * context.canvas.width);
@@ -23,13 +24,13 @@ function Flake() {
 function init() {
 	canvas = document.getElementById('testCanvas');
 	context = canvas.getContext("2d");
-	
+    
 	bufferCanvas = document.createElement("canvas");
 	bufferCanvasCtx = bufferCanvas.getContext("2d");
 	bufferCanvasCtx.canvas.width = context.canvas.width;
 	bufferCanvasCtx.canvas.height = context.canvas.height;
 
-	initialize the rects
+//	initialize the rects
 	flakeTimer = setInterval(addFlake, 200);
 
 	Draw();
@@ -51,6 +52,8 @@ function blank() {
 function animate() {
 	Update();
 	Draw();
+    
+    ctx = document.getCSSCanvasContext('2d', 'animation', 300, 300);
 }
 
 function Update() {
@@ -87,3 +90,97 @@ function Draw(){
 	context.drawImage(bufferCanvas, 0,0,bufferCanvas.width, bufferCanvas.height);
 	context.restore();
 }
+
+
+
+// var gl = document.getCSSCanvasContext('experimental-webgl', 'animation', 300, 150);
+
+
+(function(){
+    
+    
+window.requestAnimationFrame = window.requestAnimationFrame ||
+                               window.webkitRequestAnimationFrame;
+var supported = true;
+
+if (!!!document.getCSSCanvasContext) {
+  document.querySelector('#notsupport').style.display = 'block';
+  supported = false;
+}
+
+function correctPixelRatio(ctx, container) {
+    var canvas = ctx.canvas;
+	var ratio =  window.devicePixelRatio / ctx.webkitBackingStorePixelRatio;
+
+	var oldWidth = canvas.width;
+	var oldHeight = canvas.height;
+
+	canvas.width = oldWidth * ratio;
+	canvas.height = oldHeight * ratio;
+
+	canvas.style.width = oldWidth + 'px';
+	canvas.style.height = oldHeight + 'px';
+
+  //ctx.scale(ratio, ratio);
+
+	return ratio;
+}
+
+var computedStyle = window.getComputedStyle(document.querySelector('#container'));
+var WIDTH = parseInt(computedStyle.width);
+var HEIGHT = parseInt(computedStyle.height);
+
+if (supported) {
+  var ctx = document.getCSSCanvasContext('2d', 'animation', WIDTH, HEIGHT);
+  var canvas = ctx.canvas;
+} else {
+  var canvas = document.createElement('canvas');
+  var ctx = canvas.getContext('2d');
+}
+var ratio = correctPixelRatio(ctx);
+
+WIDTH = ctx.canvas.width;
+HEIGHT = ctx.canvas.height;
+
+//var rotation = 1;
+function draw(time) {
+	ctx.clearRect(0, 0, WIDTH, HEIGHT);
+	
+	var center = [WIDTH / 2, HEIGHT / 2];
+	var PADDING = 0; // px
+	var R = Math.min(WIDTH, HEIGHT) / 2 - PADDING;
+
+	var data = [75, 68, 32, 95];
+	var colors = ["red", "black", "blue", "#ffcc00"];
+
+	var lastPosition = 0;
+	var total = data.reduce(function(previousValue, currentValue, index, array) {
+	  return previousValue + currentValue
+	});
+
+	for (var i = 0; i < data.length; ++i) {
+	  ctx.fillStyle = colors[i];
+	  ctx.beginPath();
+	  ctx.moveTo(center[0], center[1]);
+	  ctx.arc(center[0], center[1], R, lastPosition,
+	          lastPosition + (Math.PI * 2 * (data[i] / total)), false);
+	  ctx.lineTo(center[0], center[1]);
+	  ctx.fill();
+	  lastPosition += Math.PI * 2 * (data[i] / total);
+	}
+	
+	ctx.translate(center[0], center[1]);
+  //ctx.rotate(rotation / time);
+  ctx.rotate(0.05);
+  ctx.translate(-center[0], -center[1]);
+  //rotation++;
+
+  window.requestAnimationFrame(draw);
+}
+
+window.requestAnimationFrame(draw);
+
+var container = document.querySelector('#canvas-container');
+container.insertBefore(canvas, container.firstElementChild);
+    
+})();
